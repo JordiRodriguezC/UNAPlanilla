4725f934e35e7e66f52021aba14d44c67103e18d
josepablobermudezm <53883209+josepablobermudezm@users.noreply.github.com>
2019-10-28 15:56:25 -0600

    adfs

--- UnaPlanilla2/src/unaplanilla2/service/TipoPlanillaService.java
+++ UnaPlanilla2/src/unaplanilla2/service/TipoPlanillaService.java
@@ -6,6 +6,8 @@
 package unaplanilla2.service;
 
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
@@ -16,6 +18,7 @@ import javax.persistence.Query;
 //import unaplanilla2.model.Empleado;
 import unaplanilla2.model.EmpleadoDto;
 import unaplanilla2.model.TipoPlanillaDto;
+import unaplanilla2.util.Request;
 //import unaplanilla2.util.EntityManagerHelper;
 import unaplanilla2.util.Respuesta;
 
@@ -24,6 +27,59 @@ import unaplanilla2.util.Respuesta;
  * @author Carlos
  */
 public class TipoPlanillaService {
+    //EntityManager em = EntityManagerHelper.getInstance().getManager();
+    private EntityTransaction et;
 
+    public Respuesta getTipoPlanilla(Long id) {
+        try {
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("id", id);
+            Request request = new Request("TipoPlanillaController/planilla", "/{id}", parametros);
+            request.get();
+
+            if (request.isError()) {
+                return new Respuesta(false, request.getError(), "");
+            }
+
+            EmpleadoDto empleado = (EmpleadoDto) request.readEntity(EmpleadoDto.class);
+            return new Respuesta(true, "", "", "Planilla", empleado);
+        } catch (Exception ex) {
+            Logger.getLogger(EmpleadoService.class.getName()).log(Level.SEVERE, "Error obteniendo la planilla [" + id + "]", ex);
+            return new Respuesta(false, "Error obteniendo el empleado.", "getPlanilla " + ex.getMessage());
+        }
+    }
+
+    
+        public Respuesta guardarTipoPlanilla(TipoPlanillaDto tipoPlanillaDto) {
+        try {
+            Request request = new Request("TipoPlanillaController/guardar");
+            request.post(tipoPlanillaDto);
+            if (request.isError()) {
+                return new Respuesta(false, request.getError(), "");
+            }
+            EmpleadoDto empleadoDto = (EmpleadoDto) request.readEntity(EmpleadoDto.class);
+            return new Respuesta(true, "", "", "TipoPlanilla", empleadoDto);
+        } catch (Exception ex) {
+            Logger.getLogger(EmpleadoService.class.getName()).log(Level.SEVERE, "Error guardando la planilla.", ex);
+            return new Respuesta(false, "Error guardando el Planilla.", "guardarPlanilla " + ex.getMessage());
+        }
+    }
+
+    public Respuesta eliminarTipoPlanilla(Long id) {
+        try {
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("id", id);
+            Request request = new Request("TipoPlanillaController/planilla","/{id}",parametros);
+            request.delete();
+
+            if (request.isError()) {
+                return new Respuesta(false, request.getError(), "");
+            }
+            return new Respuesta(true, "", "");
+        } catch (Exception ex) {
+            Logger.getLogger(EmpleadoService.class.getName()).log(Level.SEVERE, "Error eliminando el tipo planilla.", ex);
+            return new Respuesta(false, "Error eliminando el tipo Planilla.", "eliminarTipoPlanilla " + ex.getMessage());
+        }
+    }
     
 }
